8a71473b123a0d77000863c9eedee80f488f9368
xiaolunan <rxdqq@994395421>
2020-05-26 15:34:57 +0800

    main add view.

---
+++ app/src/main/java/com/example/uploadtest/utils/dpToPxUtils.java
@@ -0,0 +1,10 @@
+package com.example.uploadtest.utils;
+
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+public class dpToPxUtils {
+    public static float dpToPx(int dp) {
+        return TypedValue.applyDimension(1, (float)dp, Resources.getSystem().getDisplayMetrics());
+    }
+}
